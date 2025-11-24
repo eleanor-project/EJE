3621a0a543fbc5ead0190340f27fa2a9a07015a9
Excelsior2026 <billparris@gmail.com>
2025-11-24 07:04:36 -0800

    Implement CriticRegistry class for critics management
    Added a CriticRegistry class to manage official critics.

--- src/eje/core/critic_registry.py
+++ src/eje/core/critic_registry.py
@@ -1,2 +1,17 @@
 class CriticRegistry:
-    pass
+    """
+    Registry of all official critics included with EJE.
+    Useful for inspector dashboards, metadata, and plugin validation.
+    """
+
+    def __init__(self):
+        self._critics = {}
+
+    def register(self, name, cls):
+        self._critics[name] = cls
+
+    def get(self, name):
+        return self._critics.get(name)
+
+    def list(self):
+        return list(self._critics.keys())
